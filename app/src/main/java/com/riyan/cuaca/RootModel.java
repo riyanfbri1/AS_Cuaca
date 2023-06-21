055fc793860c523263a08af16ee553642b2adff6
riyanfbri1 <92567991+riyanfbri1@users.noreply.github.com>
2023-06-21 14:15:06 +0700

    Initial commit CUACA

---
+++ app/src/main/java/com/riyan/cuaca/RootModel.java
@@ -0,0 +1,21 @@
+package com.riyan.cuaca;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RootModel {
+    @SerializedName("list")
+    private List<ListModel> listModelList;
+
+    public List<ListModel> getListModelList() {
+        return listModelList;
+    }
+
+    public void setListModelList(List<ListModel> listModelList) {
+        this.listModelList = listModelList;
+    }
+
+    public RootModel() {
+    }
+}
